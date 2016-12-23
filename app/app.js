c2ae098d581557eeb1170f25f0ec68b89dca33b0
Thuy <tpham045@hotmail.com>
2016-12-23 03:27:45 +0000

    Add webflow UI files

---
+++ app/app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/public'));
+
+var server = app.listen(process.env.PORT, function() {
+    console.log('App is running. Go to: https://for-client-node-tpham045.c9users.io');
+});
